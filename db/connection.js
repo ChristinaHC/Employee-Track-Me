32a1a1f2931ac3c649f26236110a9b5c014b5ac3
Christina Crabtree <borrowedandblue@gmail.com>
2022-01-22 16:05:16 -0500

    setting up database

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Ichigo!NekoinABox",
+        database: "employee_tracker"
+    },
+    console.log("Connected to the employee_tracker database!")
+);
+
+module.exports = db;
